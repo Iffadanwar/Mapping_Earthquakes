0df4c42898f0dedb15d4e65117bc9d67b9c97511
Iffadanwar <72320831+Iffadanwar@users.noreply.github.com>
2022-02-20 10:37:50 -0600

    cool

--- Mapping_GeoJSON_Points/static/JS/logic.js
+++ Mapping_GeoJSON_Points/static/JS/logic.js
@@ -1,23 +1,41 @@
-// Create the map object with center and zoom level.
-let map = L.map("mapid").setView([30,30], 2);
-
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY,
 });
-streets.addTo(map);
 
-let airportData = "https://raw.githubusercontent.com/iffadanwar/Mapping_Earthquakes/Mapping_GeoJSON_Points/static/JS/majorAirports.json";
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY,
+});
+
+let baseMaps = {
+    Street: streets,
+    Dark: dark
+}
+
+let map = L.map("mapid", {
+    conter:[30,30],
+    zoom: 2,
+    layers: [streets]
+})
+L.control.layers(baseMaps).addTo(map);
+
+let airportData = "https://raw.githubusercontent.com/iffadanwar/Mapping_Earthquakes/main/majorAirports.json";
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
     console.log(data);
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
-});
+//   // Creating a GeoJSON layer with the retrieved data.
+//   L.geoJSON(data, {
+//       onEachFeature: function(data) {
+//           console.log(data);
+    L.geoJson(data).addTo(map);
 
+});
+//-----------------------------------------------------------------------------
 // // Add GeoJSON data.
 // let sanFranAirport =
 // {"type":"FeatureCollection","features":[{
